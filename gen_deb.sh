77db3c7a6a2ad7606dc8fe73be93837b39d76bc2
Benjamin Abendroth <braph93@gmx.de>
2024-06-02 15:36:01 +0200

    First packages

---
+++ gen_deb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+unset CDPATH
+set -u +o histexpand
+set -e
+
+[[ -e 'nbfc-linux' ]] || \
+  git clone https://github.com/nbfc-linux/nbfc-linux
+
+cd nbfc-linux
+VERSION=$(grep AC_INIT configure.ac | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' )
+./autogen.sh
+./configure --prefix=/usr --sysconfdir=/etc --bindir=/usr/bin
+make
+rm -rf build
+mkdir build
+make DESTDIR=build install
+cd build
+fpm -s dir -t deb -n nbfc-linux -v $VERSION *
+mv *.deb ../..
